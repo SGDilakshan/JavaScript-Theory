7f3ced4c44caf000e7bf6b1a3cc3099921fe436a
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-12 12:18:12 +0530

    Update the Relational Operations in JavaScript

---
+++ 10_Relational Operations in JavaScript/js/script.js
@@ -0,0 +1,24 @@
+// Relational Operations in JavaScript
+
+/*
+>  greater than
+<  less than
+>= greater than or equal to
+<= less than or equal to
+*/
+
+// Declare two variables a and b
+let a = 10;
+let b = 20;
+
+// Check if a is greater than b
+console.log("Greater            : ", a > b);  // false, because 10 is not greater than 20
+
+// Check if a is less than b
+console.log("Lesser             : ", a < b);  // true, because 10 is less than 20
+
+// Check if a is greater than or equal to b
+console.log("Greater than equal : ", a >= b);  // false, because 10 is not greater than or equal to 20
+
+// Check if a is less than or equal to b
+console.log("Lesser than equal  : ", a <= b);  // true, because 10 is less than or equal to 20
