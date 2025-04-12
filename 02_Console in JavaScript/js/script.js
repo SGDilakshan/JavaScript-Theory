a916df32b64e045552122704b6f6a6d930469aff
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-12 11:43:16 +0530

    Update the Console in JavaScript

---
+++ 02_Console in JavaScript/js/script.js
@@ -0,0 +1,20 @@
+//alert("Welcome");
+
+console.log("Welcome to Java Script");
+console.log(13456);
+console.log(18.25);
+console.log(true);
+console.log([58,78,96,35]);
+console.log([{fname:'Dilakshan',age:25}]);
+console.table([{fname:'Dilakshan',age:25}]);
+console.error("Custom Sample Error");
+console.warn("Custom Sample Error");
+console.clear();
+console.time("Timer");
+for(i=0;i<10;i++){
+    console.log(i);
+}
+console.timeEnd("Timer");
+
+
+
