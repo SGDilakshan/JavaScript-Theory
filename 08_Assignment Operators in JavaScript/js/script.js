522a5f261c957d4dbe281271854b2b435aa17d8c
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-12 12:16:56 +0530

    Update the Assignment Operators in JavaScript

---
+++ 08_Assignment Operators in JavaScript/js/script.js
@@ -0,0 +1,22 @@
+// Assignment Operators in JavaScript
+
+let a = 10;  
+// Addition Assignment (a += 5 is equivalent to a = a + 5)
+a += 5;  // Adds 5 to 'a'
+console.log(a);  // Outputs 15
+
+// Subtraction Assignment (a -= 5 is equivalent to a = a - 5)
+a -= 5;  // Subtracts 5 from 'a'
+console.log(a);  // Outputs 10
+
+// Multiplication Assignment (a *= 5 is equivalent to a = a * 5)
+a *= 5;  // Multiplies 'a' by 5
+console.log(a);  // Outputs 50
+
+// Division Assignment (a /= 5 is equivalent to a = a / 5)
+a /= 5;  // Divides 'a' by 5
+console.log(a);  // Outputs 10
+
+// Modulus Assignment (a %= 5 is equivalent to a = a % 5)
+a %= 5;  // Sets 'a' to the remainder when 'a' is divided by 5
+console.log(a);  // Outputs 0 (since 10 % 5 equals 0)
