62880ef776e79ee7dcc3966d693f4b4eac49d1c2
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-14 01:54:26 +0530

    Update the Concat in JavaScript

---
+++ 50_Concat in JavaScript/js/script.js
@@ -0,0 +1,18 @@
+// Concat in JavaScript
+
+const a=[10,20,30];
+const b=[40,50,60];
+const c=[70,80,90];
+
+let d=a.concat(b);
+console.log(d);
+
+d=a.concat(b,c);
+console.log(d);
+
+d=a.concat(b,c,25,35,45);
+console.log(d);
+
+d=a.concat(b,c,25,35,45,['a','b','c']);
+console.log(d);
+console.table(d);
