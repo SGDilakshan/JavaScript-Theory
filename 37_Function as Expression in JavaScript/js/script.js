41458570cfbca7151cd26787998e2c2a80fff6a3
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-14 01:33:44 +0530

    Update the Function as Expression in JavaScript

---
+++ 37_Function as Expression in JavaScript/js/script.js
@@ -0,0 +1,12 @@
+// Function as Expression in JavaScript
+
+// Defining a function as an expression and assigning it to a variable
+const add = function(a, b) {
+    return a + b; // Returns the sum of a and b
+};
+
+// Logging the function reference (not executing it)
+console.log(add);
+
+// Calling the function with arguments and logging the result
+console.log(add(25, 78));
